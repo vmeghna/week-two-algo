35d3e565c3fd3fcc36d8eaba01730760b0e04880
vmeghna <shrimeghna97@gmail.com>
2023-07-11 21:32:05 +0530

    javascript basics

---
+++ script.js
@@ -0,0 +1,49 @@
+// 1.Write a JavaScript function to check if a given number is even or odd.
+
+
+// 2.Write a JavaScript program to calculate the factorial of a number.
+// 3.Write a JavaScript program to find the sum of all numbers in each array, excluding any numbers that are less than 0.
+// 4.Write a JavaScript program to check if a given number, is a perfect square.
+// 5. Write a JavaScript function that accepts a string as a parameter and counts the number of vowels within the string.
+// Note : As the letter 'y' can be regarded as both a vowel and a consonant, we do not count 'y' as vowel here.
+// Example string: 'The quick brown fox'
+// Expected Output: 5
+
+// 6.Write a JavaScript function to add rows to a table.
+// Sample HTML file:
+// <!DOCTYPE html>
+// <html><head><meta charset=utf-8 />
+// <title>Insert row in a table - w3resource</title>
+// </head><body>
+// <table id="sampleTable" border="1">
+// <tr><td>Row1 cell1</td>
+// <td>Row1 cell2</td></tr>
+// <tr><td>Row2 cell1</td>
+// <td>Row2 cell2</td></tr>
+// </table><br>
+// <input type="button" onclick="insert_Row()" value="Insert row">
+// </body></html>
+
+// 7. Write a JavaScript function to print an integer with thousands separated by commas.
+// Test Data :
+// console.log(thousands_separators(1000));
+// "1,000"
+// console.log(thousands_separators(10000.23));
+// "10,000.23"
+// console.log(thousands_separators(100000));
+// "100,000"
+
+// 8. Write a JavaScript program to find the Armstrong numbers of 3 digits.
+// Note : An Armstrong number of three digits is an integer such that the sum of the cubes of its digits is equal to the number itself. For example, 371 is an Armstrong number since 3**3 + 7**3 + 1**3 = 371.
+
+// 9. Write a JavaScript program to check if two given integers have opposite signs.
+// Test Data:
+// (100, -100) -> "Signs are opposite"
+// (100, 100) -> "Signs are not opposite"
+// ('100, 100) -> "Parameters value must be number!"
+
+// 10. In an array every element appears twice except for one. Write a JavaScript program to find the non-repeated element in an array using bit manipulation.
+// Test Data:
+// ([1]) -> 1
+// ([1, 2, 3]) -> 0 [All elements are non- repeated]
+// [1, 2, 8, 3, 1, 2, 3, 8, 6, 6, 7] -> 7
